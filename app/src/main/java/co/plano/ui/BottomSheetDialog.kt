5ebf88892bd8ddf1fdbae86e5d917740be93703c
APURVA <shahnawazhasan03@gmail.com>
2020-10-27 09:56:18 +0530

    Tabs of child activity report

---
+++ app/src/main/java/co/plano/ui/BottomSheetDialog.kt
@@ -0,0 +1,25 @@
+package co.plano.ui
+
+import android.R
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.Nullable
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+
+class BottomSheetDialog : BottomSheetDialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        @Nullable container: ViewGroup?,
+        @Nullable savedInstanceState: Bundle?
+    ): View? {
+        /*val view: View = inflater.inflate(
+            R.layout.b,
+            container, false
+        )*/
+
+        return view
+    }
+}
